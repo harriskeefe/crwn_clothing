af6a79eaf1a4755c5ebabc19e005cb3c0b8ef829
keefeharris <keefeharrisjr@gmail.com>
2022-08-13 15:08:13 -0400

    FromInput component was utilized.

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,18 +1,24 @@
 import {
   signInWithGooglePopup,
-  createUserProfileDocument,
+  createUserDocumentFromAuth
 } from '../../utils/firebase/firebase.utils';
 
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component'
+
 const SignIn = () => {
+  // button points to this async function
+  // the response is destructured to the authorization object from the google popup
+  // createUserDocumentFromAuth is called to with the destructured user variable from auth object, go to firebase utils
   const logGoogleUser = async () => {
-    const response = await signInWithGooglePopup();
-    createUserProfileDocument(response);
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = createUserDocumentFromAuth(user);
   };
 
   return (
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <SignUpForm />
     </div>
   );
 };
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,11 +1,23 @@
+// firebase app
 import { initializeApp } from 'firebase/app';
+
+// firebase authorization
 import {
   getAuth,
-  signInWithRedirect,
   signInWithPopup,
   GoogleAuthProvider,
+  createUserWithEmailAndPassword
 } from 'firebase/auth';
 
+// firebase database
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc
+} from 'firebase/firestore';
+
+// firebase configuration for CRUD operations
 const firebaseConfig = {
   apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
   authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
@@ -17,17 +29,68 @@ const firebaseConfig = {
 
 const firebaseApp = initializeApp(firebaseConfig);
 
+// google class provider and authorization object are paramaters for the google popup
 const provider = new GoogleAuthProvider();
-
 provider.setCustomParameters({
   prompt: 'select_account',
 });
-
-export const createUserProfileDocument = async (userAuth, additionalData) => {
-  if (!userAuth) return;
-
-  console.log(userAuth);
-};
-
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+// takes data from sign-in component(authentication object is userAuth) and stores in firestore
+export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
+  if(!userAuth) return;
+  // at this time, this document doesn't exist but is now referenced by the 'users' path and the uid
+  // we can set and get data based on this reference
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  // lets get the data from this document with a snapshot
+  const userSnapshot = await getDoc(userDocRef);
+
+  // if user data does not exist
+  // set data from auth object 
+  // else return data
+  if(!userSnapshot.exists()) {
+    // create/destructure variables to set a user document 
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    
+    // user document is set in the database
+    try {
+      await setDoc(userDocRef, {
+      displayName,
+      email,
+      createdAt,
+      ...additionalInformation
+    });
+  } catch(error) {
+      console.log('error creating the user', error.message);
+    }
+    console.log(userSnapshot);
+    return userSnapshot;
+  }
+}
+
+/*********
+ Authenticating email and password sign up (not google popup)
+ *********/
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if(!(email || password)) return
+  return await createUserWithEmailAndPassword(auth, email, password);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
